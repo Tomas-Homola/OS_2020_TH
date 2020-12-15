fbb0c95817c532a2cb5938610b69f5d9b6824e21
Tomas-Homola <homola.tom6@gmail.com>
2020-12-15 18:11:26 +0100

    Prednaska 13

---
+++ python/closure.py
@@ -0,0 +1,17 @@
+
+def get_adder(n):
+
+    def to_ret(x):
+
+        return n + x
+
+    return to_ret
+
+def add_debug(f):
+
+    def to_ret(x):
+
+        print(f'volame {f.__name__}({x})')
+        return f(x)
+
+    return to_ret
